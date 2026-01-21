2c8f6297ba23e6575c59e21ff6d596d636e134f7
Madhu Sudhan <2300031210@kluniversity.in>
2026-01-21 16:10:19 +0530

    Backtracking Problem

---
+++ Dynamic Programming/Max Sum without Adjacents .cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://www.geeksforgeeks.org/problems/max-sum-without-adjacents2430/1
+// Time Complexity: O(n)
+
+// User function template for C++
+class Solution {
+public:
+    // calculate the maximum sum with out adjacent
+    int findMaxSum(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        if(n == 1) return arr[0];
+        vector<int> dp(n+1);
+        dp[0] = arr[0];
+        dp[1] = max(arr[0], arr[1]);
+        for(int i = 2; i < n; i++) dp[i] = max(dp[i-1], arr[i] + dp[i-2]);
+        return dp[n-1];
+    }
+};
